e148446e91e1eda5b0437219c81fd6fec96abee2
Forrest <fcrawford1992@gmail.com>
2024-06-10 17:48:07 -0400

    Add project files.

---
+++ Direct2DinWPF/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Direct2DinWPF
+{
+    class Sample
+    {
+        //[DllImport(“DxfDrawer.dll”)]
+        //internal static extern IntPtr Initialize(IntPtr hwnd, int width, int height);
+        //[DllImport(“DxfDrawer.dll”)]
+        //internal static extern void Render();
+        //[DllImport(“DxfDrawer.dll”)]
+        //internal static extern void Cleanup();
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void d3dImage_IsFrontBufferAvailableChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+    }
+}
